6e5f572dc8c7723631fcd87498cff46b49f1d19c
Hernán Marrapodi <hernanmarra2002@gmail.com>
2023-11-11 18:00:08 -0300

    Pase de precommit

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/songs/network/SongsAPI.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/songs/network/SongsAPI.kt
@@ -1,15 +1,13 @@
 package ar.edu.unlam.mobile.scaffold.data.songs.network
 
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface SongsAPI {
 
-    @Headers("Authorization: Bearer BQBAWrrZHs1LSa5h2HborWrPVAB0vBhyh-J5HmiMFY4gWX2r5w3N6zRpNwmCDOIxLv8ApT9qy9ij6rLrhpustUxHaQva97oO7rIYgb8BfYH0HXPeY-I")
     @GET("v1/search")
     suspend fun search(
         @Query("q") query: String,
-        @Query("type") type: String = "track"
+        @Query("type") type: String = "track",
     ): SongsApiModel
 }
--- app/src/main/java/ar/edu/unlam/mobile/scaffold/data/track/di/TrackDataProvider.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/data/track/di/TrackDataProvider.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class TrackDataProvider {
+object TrackDataProvider {
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(BearerTokenInterceptor())
         .build()
